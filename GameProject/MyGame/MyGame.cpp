e8a6d0647b4eab24288ba9b204623194ffa3989f
KyouGenSo <k023g0083@g.neec.ac.jp>
2025-12-03 15:04:43 +0900

    update: MyGame初期化でGlobalVariablesのJsonファイル読み込み追加
    - Boss.jsonでHitEffectDurationの値を0.1から0.15に変更
    - BossBullet.jsonでDamageの値を10.0から5.0に変更
    - DashEffect.jsonでLerpSpeedの値を35.0から33.5に変更

--- GameProject/MyGame/MyGame.cpp
+++ GameProject/MyGame/MyGame.cpp
@@ -16,163 +16,167 @@
 
 void MyGame::Initialize()
 {
-  winApp_->SetWindowSize(1920, 1080);
+    winApp_->SetWindowSize(1920, 1080);
 
-  winApp_->SetWindowTitle(L"LE3B_12_キョウ_ゲンソ_Slash");
+    winApp_->SetWindowTitle(L"LE3B_12_キョウ_ゲンソ_Slash");
 
-  TakoFramework::Initialize();
+    TakoFramework::Initialize();
 
 #pragma region 汎用機能初期化-------------------------------------------------------------------------------------------------------------------
-  // 入力クラスの初期化
-  Input::GetInstance()->Initialize(winApp_);
+    // 入力クラスの初期化
+    Input::GetInstance()->Initialize(winApp_);
 
-  // オーディオの初期化
-  Audio::GetInstance()->Initialize("resources/Sound/");
+    // オーディオの初期化
+    Audio::GetInstance()->Initialize("resources/Sound/");
 
 #pragma endregion
 
-  // シーンの初期化
-  sceneFactory_ = new SceneFactory();
-  SceneManager::GetInstance()->SetSceneFactory(sceneFactory_);
-  SceneManager::GetInstance()->ChangeScene("title", 0.0f);
+    // シーンの初期化
+    sceneFactory_ = new SceneFactory();
+    SceneManager::GetInstance()->SetSceneFactory(sceneFactory_);
+    SceneManager::GetInstance()->ChangeScene("title", 0.0f);
 
-  TextureManager::GetInstance()->LoadTexture("white.png");
-  TextureManager::GetInstance()->LoadTexture("black.png");
-  TextureManager::GetInstance()->LoadTexture("circle.png");
-  TextureManager::GetInstance()->LoadTexture("my_skybox.dds");
-  TextureManager::GetInstance()->LoadTexture("title_text/title_text_1.png");
-  TextureManager::GetInstance()->LoadTexture("title_text/title_text_2.png");
-  TextureManager::GetInstance()->LoadTexture("title_text/title_text_3.png");
-  TextureManager::GetInstance()->LoadTexture("title_text/title_text_4.png");
-  TextureManager::GetInstance()->LoadTexture("title_text/title_text_5.png");
-  TextureManager::GetInstance()->LoadTexture("title_text/title_text_6.png");
-  TextureManager::GetInstance()->LoadTexture("title_text/title_text_7.png");
-  TextureManager::GetInstance()->LoadTexture("title_text/title_text_8.png");
-  TextureManager::GetInstance()->LoadTexture("title_text/title_text_9.png");
-  TextureManager::GetInstance()->LoadTexture("title_text/title_text_10.png");
-  TextureManager::GetInstance()->LoadTexture("title_button.png");
-  TextureManager::GetInstance()->LoadTexture("game_button_text.png");
-  TextureManager::GetInstance()->LoadTexture("gameClear_Text.png");
-  TextureManager::GetInstance()->LoadTexture("gameOver_Text.png");
+    // テクスチャの読み込み
+    TextureManager::GetInstance()->LoadTexture("white.png");
+    TextureManager::GetInstance()->LoadTexture("black.png");
+    TextureManager::GetInstance()->LoadTexture("circle.png");
+    TextureManager::GetInstance()->LoadTexture("my_skybox.dds");
+    TextureManager::GetInstance()->LoadTexture("title_text/title_text_1.png");
+    TextureManager::GetInstance()->LoadTexture("title_text/title_text_2.png");
+    TextureManager::GetInstance()->LoadTexture("title_text/title_text_3.png");
+    TextureManager::GetInstance()->LoadTexture("title_text/title_text_4.png");
+    TextureManager::GetInstance()->LoadTexture("title_text/title_text_5.png");
+    TextureManager::GetInstance()->LoadTexture("title_text/title_text_6.png");
+    TextureManager::GetInstance()->LoadTexture("title_text/title_text_7.png");
+    TextureManager::GetInstance()->LoadTexture("title_text/title_text_8.png");
+    TextureManager::GetInstance()->LoadTexture("title_text/title_text_9.png");
+    TextureManager::GetInstance()->LoadTexture("title_text/title_text_10.png");
+    TextureManager::GetInstance()->LoadTexture("title_button.png");
+    TextureManager::GetInstance()->LoadTexture("game_button_text.png");
+    TextureManager::GetInstance()->LoadTexture("gameClear_Text.png");
+    TextureManager::GetInstance()->LoadTexture("gameOver_Text.png");
 
-  spriteBasicOnresizeId_ = winApp_->RegisterOnResizeFunc(std::bind(&SpriteBasic::OnResize, SpriteBasic::GetInstance(), std::placeholders::_1));
+    // GlobalVariablesのJsonファイル読み込み
+    GlobalVariables::GetInstance()->LoadFiles();
 
-  // GPUパーティクルの初期化
-  GPUParticle::GetInstance()->Initialize(dx12_, defaultCamera_);
+    // SpriteBasicのリサイズコールバック関数登録
+    spriteBasicOnresizeId_ = winApp_->RegisterOnResizeFunc(std::bind(&SpriteBasic::OnResize, SpriteBasic::GetInstance(), std::placeholders::_1));
+
+    // GPUパーティクルの初期化
+    GPUParticle::GetInstance()->Initialize(dx12_, defaultCamera_);
 }
 
 void MyGame::Finalize()
 {
-  winApp_->UnregisterOnResizeFunc(spriteBasicOnresizeId_);
+    winApp_->UnregisterOnResizeFunc(spriteBasicOnresizeId_);
 
-  // GPUパーティクルの解放
-  GPUParticle::GetInstance()->Finalize();
+    // GPUパーティクルの解放
+    GPUParticle::GetInstance()->Finalize();
 
-  // Audioの解放
-  Audio::GetInstance()->Finalize();
+    // Audioの解放
+    Audio::GetInstance()->Finalize();
 
-  // 入力クラスの解放
-  Input::GetInstance()->Finalize();
+    // 入力クラスの解放
+    Input::GetInstance()->Finalize();
 
-  TakoFramework::Finalize();
+    TakoFramework::Finalize();
 }
 
 void MyGame::Update()
 {
-  // カメラの更新
-  defaultCamera_->Update();
+    // カメラの更新
+    defaultCamera_->Update();
 
-  // 入力情報の更新
-  Input::GetInstance()->Update();
+    // 入力情報の更新
+    Input::GetInstance()->Update();
 
-  // F11キーでフルスクリーン切り替え
-  if (Input::GetInstance()->TriggerKey(DIK_F11))
-  {
-    ToggleFullScreen();
-  }
+    // F11キーでフルスクリーン切り替え
+    if (Input::GetInstance()->TriggerKey(DIK_F11)) {
+        ToggleFullScreen();
+    }
 
-  // GPUパーティクルの更新
-  GPUParticle::GetInstance()->Update();
+    // GPUパーティクルの更新
+    GPUParticle::GetInstance()->Update();
 
-  TakoFramework::Update();
+    TakoFramework::Update();
 
-  //　サウンドの更新
-  Audio::GetInstance()->Update();
+    //　サウンドの更新
+    Audio::GetInstance()->Update();
 
-  // ゲームパッドの状態をリフレッシュ
-  Input::GetInstance()->RefreshGamePadState();
+    // ゲームパッドの状態をリフレッシュ
+    Input::GetInstance()->RefreshGamePadState();
 }
 
 void MyGame::Draw()
 {
-  /// ============================================= ///
-  /// ------------------シーン描画-------------------///
-  /// ============================================= ///
+    /// ============================================= ///
+    /// ------------------シーン描画-------------------///
+    /// ============================================= ///
 
-  //ポストエフェクト適用対象のレンダーテクスチャを描画先に設定
-  dx12_->SetEffectRenderTexture();
+    //ポストエフェクト適用対象のレンダーテクスチャを描画先に設定
+    dx12_->SetEffectRenderTexture();
 
-  // テクスチャ用のsrvヒープの設定
-  SrvManager::GetInstance()->BeginDraw();
+    // テクスチャ用のsrvヒープの設定
+    SrvManager::GetInstance()->BeginDraw();
 
-  SceneManager::GetInstance()->Draw();
+    SceneManager::GetInstance()->Draw();
 
-  GPUParticle::GetInstance()->Draw();
+    GPUParticle::GetInstance()->Draw();
 
-  Draw2D::GetInstance()->Draw();
+    Draw2D::GetInstance()->Draw();
 
-  /// ===================================================== ///
-  /// ------------------ポストエフェクト描画-------------------///
-  /// ===================================================== ///
+    /// ===================================================== ///
+    /// ------------------ポストエフェクト描画-------------------///
+    /// ===================================================== ///
 
-    // ポストエフェクトの描画
-  PostEffectManager::GetInstance()->Draw();
+      // ポストエフェクトの描画
+    PostEffectManager::GetInstance()->Draw();
 
-  /// ===================================================== ///
-  /// ------------ポストエフェクト非適用対象の描画---------------///
-  /// ===================================================== ///
-  // ポストエフェクト非適用対象のレンダーテクスチャを描画先に設定
-  dx12_->SetNonEffectRenderTexture();
+    /// ===================================================== ///
+    /// ------------ポストエフェクト非適用対象の描画---------------///
+    /// ===================================================== ///
+    // ポストエフェクト非適用対象のレンダーテクスチャを描画先に設定
+    dx12_->SetNonEffectRenderTexture();
 
-  // シーンの描画
-  SceneManager::GetInstance()->DrawWithoutEffect();
+    // シーンの描画
+    SceneManager::GetInstance()->DrawWithoutEffect();
 
-  TransitionManager::GetInstance()->Draw();
+    TransitionManager::GetInstance()->Draw();
 
-  Draw2D::GetInstance()->Reset();
+    Draw2D::GetInstance()->Reset();
 
-  /// ============================================= ///
-  /// ---------最終結果をスワップチェーンに描画---------///
-  /// ============================================= ///
-  bool isDrawToSwapChain = true;
+    /// ============================================= ///
+    /// ---------最終結果をスワップチェーンに描画---------///
+    /// ============================================= ///
+    bool isDrawToSwapChain = true;
 
 #ifdef _DEBUG
-  isDrawToSwapChain = !DebugUIManager::GetInstance()->IsWindowVisible("GameViewport");
+    isDrawToSwapChain = !DebugUIManager::GetInstance()->IsWindowVisible("GameViewport");
 #endif
 
-  PostEffectManager::GetInstance()->DrawFinalResult(isDrawToSwapChain);
+    PostEffectManager::GetInstance()->DrawFinalResult(isDrawToSwapChain);
 
 
-  /// ========================================= ///
-  ///-------------------ImGui-------------------///
-  /// ========================================= ///
+    /// ========================================= ///
+    ///-------------------ImGui-------------------///
+    /// ========================================= ///
 #ifdef _DEBUG
 
-  imguiManager_->Begin();
+    imguiManager_->Begin();
 
-  TakoFramework::Draw();
+    TakoFramework::Draw();
 
-  //SceneManager::GetInstance()->DrawImGui();
+    //SceneManager::GetInstance()->DrawImGui();
 
-  Draw2D::GetInstance()->ImGui();
+    Draw2D::GetInstance()->ImGui();
 
-  imguiManager_->End();
+    imguiManager_->End();
 
-  //imguiの描画
-  imguiManager_->Draw();
+    //imguiの描画
+    imguiManager_->Draw();
 #endif
 
 
-  // 描画後の処理
-  dx12_->EndDraw();
+    // 描画後の処理
+    dx12_->EndDraw();
 }
